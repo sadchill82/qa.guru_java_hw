48fe50a3aecb0cfbc980a0cbbc9ff1d96857c7d2
sadchill82 <nurmuhametoffislam@yandex.ru>
2025-01-30 00:18:10 +0300

    add TestBase, RegistrationPage, CalendarComponent, ResultTableComponent

---
+++ src/test/java/lesson6/tests/TestBase.java
@@ -0,0 +1,14 @@
+package lesson6.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+}
